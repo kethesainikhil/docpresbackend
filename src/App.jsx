20c0743a4f0d017f0ecf1406d54eeb4dff4a8345
kethesainikhil <nikhilkethe007@gmail.com>
2024-03-13 20:42:02 +0530

    frontend part complete

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+
+import React from 'react'
+import axios from 'axios'
+import Home from './Home'
+function App() {
+
+  return (
+    <div>
+      <Home />
+    </div>
+  )
+}
+
+export default App
